f6da9bf5069d3a98a4e1567fca1409c65ebc60f3
Istiaq Hossain <hossain.ict@gmail.com>
2022-04-21 14:42:17 +0600

    add add and fetch method in criteria view and add service

--- src/app/excel-data-analyser/data-analysis/criteria/services/criteria-add.service.ts
+++ src/app/excel-data-analyser/data-analysis/criteria/services/criteria-add.service.ts
@@ -1,9 +1,15 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
+import { HttpParams } from '@angular/common/http';
+import { Observable } from "rxjs";
 
 @Injectable()
 export class CriteriaAddService {
   constructor(private http: HttpClient) {
 
   }
+  public add(): Observable<any>{
+    return this.http.get("");
+  }
 }
+
--- src/app/excel-data-analyser/data-analysis/criteria/services/criteria-view.service.ts
+++ src/app/excel-data-analyser/data-analysis/criteria/services/criteria-view.service.ts
@@ -1,9 +1,13 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable()
 export class CriteriaAddService {
   constructor(private http: HttpClient) {
 
+  }
+  public fetchCriteriaList(): Observable<any>{
+    return this.http.get("S");
   }
 }
