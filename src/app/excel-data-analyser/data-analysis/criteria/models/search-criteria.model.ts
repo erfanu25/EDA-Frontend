af2c0987e3188642d8f5ebe88bd512539b239902
Istiaq Hossain <hossain.ict@gmail.com>
2022-04-27 13:22:03 +0600

    merged seperated list components

--- src/app/excel-data-analyser/data-analysis/criteria/models/search-criteria.model.ts
+++ src/app/excel-data-analyser/data-analysis/criteria/models/search-criteria.model.ts
@@ -5,3 +5,29 @@ export interface SearchCriteria {
     operator: string;
     value: string;
 }
+
+// select name,age,address,salary from employee where month=3 and salary>5000
+
+// month=3&salary>5000
+
+// [
+// 	{
+// 		"column":"month",
+// 		"operator":"eq",
+// 		"value":3,
+	
+// 	},
+// 	{
+// 		"column":"salary",
+// 		"operator":"gt",
+// 		"value":5000
+	
+// 	}
+// ]
+
+
+
+// CriteriaName:"ABC"
+// Content:JsonObject
+
+
