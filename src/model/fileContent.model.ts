a64d456b40e48eab09ef8e5da1f7e92f4c7b259c
Iftekher Majumder <iftekher.majumder@bd.ey.com>
2022-05-11 11:24:36 +0600

    export dbtable  #Please enter the commit message for your changes. Lines starting

--- src/model/fileContent.model.ts
+++ src/model/fileContent.model.ts
@@ -1,12 +1,13 @@
-import mongoose,{ Schema,Types,model} from "mongoose";
+import mongoose, { Schema, Types, model } from "mongoose";
 import { IStatus } from "./status.model";
 // 1. Create an interface representing a document in MongoDB.
 export interface IFileContent {
     name: string,
     status: IStatus,
-    createdAt :number,
-    scheduleAt : number,
-    path : string
+    mapperId: String,
+    createdAt: number,
+    scheduleAt: number,
+    path: string
 
 }
 //Create a Schema corresponding to the document interface.
@@ -14,16 +15,16 @@ const fileContentSchema = new Schema<IFileContent>({
     name: { type: String, required: true },
     status: {
         type: Types.ObjectId,
-        ref : "status" 
-        
+        ref: "status"
+
     },
+    mapperId: String,
     createdAt: { type: Number, required: true },
     scheduleAt: { type: Number, required: false },
-    path:{type : String, required:true}
-  });
+    path: { type: String, required: true }
+});
 
-  export default model.call(require('mongoose'), 'FileContent', fileContentSchema);
+export default model.call(require('mongoose'), 'FileContent', fileContentSchema);
 
  //export default mongoose.model<IFileInfo>("FileInfo", fileInfoSchema)
 
-  
